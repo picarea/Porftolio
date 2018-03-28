1ba5aa1a020fb99bb8d84f48aaa461656915ca45
RomarioGiacholari <giacholari@gmail.com>
2018-03-28 16:18:41 +0100

    added more photos

--- index.js
+++ index.js
@@ -149,3 +149,11 @@
     }
 
 })(jQuery, window, document);
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
