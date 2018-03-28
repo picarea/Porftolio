c530b8566f66d4ae51c19b6821088294d88dd623
RomarioGiacholari <giacholari@gmail.com>
2018-03-28 17:56:53 +0100

    more photos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
     $(document).ready(function() {
         $('#pinBoot').pinterest_grid({
-        no_columns: 4,
+        no_columns: 3,
         padding_x: 10,
         padding_y: 10,
         margin_bottom: 50,
